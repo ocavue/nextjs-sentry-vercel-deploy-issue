0715198e7b2f48f573a185326e489a1647c50e58
ocavue <ocavue@gmail.com>
2024-07-30 16:39:00 +1000

    Set deleteSourcemapsAfterUpload to true

--- next.config.js
+++ next.config.js
@@ -65,6 +65,6 @@ module.exports = withSentryConfig(moduleExports, {
   automaticVercelMonitors: true,
 
   sourcemaps: {
-    deleteSourcemapsAfterUpload: false,
+    deleteSourcemapsAfterUpload: true,
   },
 });
